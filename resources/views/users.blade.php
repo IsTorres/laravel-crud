c5f798e84e2ea94e8b182b2c4c6c944709806199
Ismael Torres <ismaeltorres612@gmail.com>
2024-03-04 14:12:15 +0000

    add users page

---
+++ resources/views/users.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+
+@section('content')
+
+<h2> Users </h2>
+
+<ul>
+    @foreach ($users as $user)
+        <li>
+            <a href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a>
+        </li>
+    @endforeach
+</ul>
+
+@endsection
